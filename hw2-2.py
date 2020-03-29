8301b645458a135f1d78909fd23301ae265e38a5
tongbs <tongsteven79@gmail.com>
2020-03-30 03:02:19 +0800

    likelihood and beta posterior done

--- hw2-2.py
+++ hw2-2.py
@@ -1,3 +1,25 @@
+import math
+
+def binary_count(line):
+    num_1 = 0
+    num_0 = 0
+    for i in range(len(line)):
+        if line[i] == '1':
+            num_1 +=1
+        else:
+            num_0 += 1
+    return num_1, num_0
+
+def C(n,k):
+    numerator = math.factorial(n)
+    denominator = math.factorial(k)*math.factorial(n-k)
+    ans = numerator/denominator
+    return ans
+
+def binomial(num_1, num_0):
+    p = num_1/(num_1+num_0)
+    pr = math.pow(p,num_1)*math.pow(1-p,num_0)*C(num_1+num_0,num_1)
+    return pr
 
 
 if __name__ == "__main__":
@@ -7,5 +29,15 @@ if __name__ == "__main__":
     f = open("testfile.txt","r")
     lines = f.readlines()
 
-    for line in lines:
-        print(line)
+    for i , line in enumerate(lines):
+        line = line.strip('\n')
+        print(f'case {i}: {line}')
+        num_1 , num_0 = binary_count(line)
+        print(num_1,num_0)
+        likelihood = binomial(num_1, num_0)
+        print(f'Likelihood: {likelihood}')
+        print(f'Beta prior:     a = {a} b = {b}')
+        a+=num_1
+        b+=num_0
+        print(f'Beta posterior: a = {a} b = {b}')
+        print()
