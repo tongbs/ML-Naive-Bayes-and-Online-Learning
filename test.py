3a4e77ca669a08ade11fa20ac0d0cbd2cecc7ea0
tongbs <tongsteven79@gmail.com>
2020-03-29 22:04:02 +0800

    test some useless function

--- test.py
+++ test.py
@@ -2,11 +2,36 @@ import numpy as np
 
 tb1 = np.ones((1,1,32))
 print(tb1)
-
+'''
 for i in range(1):
     for j in range(1):
         print(tb1[i,j])
 prob_c = [0.0 for i in range(10)]
 print(prob_c)
+a = np.array([8,2,3,4,5])
+b = np.argmin(a)
+print(a[b])
+'''
+a_list = [1,1,1,0,0]
+
+correct  = 0
+wrong = 0
+for i in range(len(a_list)):
+    if a_list[i] == 1:
+        correct+=1
+    else:
+        wrong+=1
+ 
+print(correct)
+
+a = []
+for i in range(10):
+	#a.append([])
+	for j in range(5):
+		a[i].append([])
+
+print(a)
 
 
+data_list = [[[] for i in range(5)] for j in range(10)]
+print(data_list)
